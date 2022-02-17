c3d1426c9a2ef81d9a39607f8f7a3055c8b6b257
Mustafa <40180893+Nostafa@users.noreply.github.com>
2022-02-17 11:11:19 +0200

    Add files via upload

--- mongoconnect.js
+++ mongoconnect.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/social', () => {
-  console.log('db connected');
+mongoose.connect("mongodb://localhost:27017/mustafa", () => {
+  console.log("db connected");
 });
